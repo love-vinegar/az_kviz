e6b7c2c2e61842d6f005372ed8f96962c973cf17
David Ployer <david.ployer1@vodafone.com>
2025-08-31 09:51:48 +0200

    added todos

---
+++ src/main/java/cz/personal/vinegar/Todos.java
@@ -0,0 +1,9 @@
+package cz.personal.vinegar;
+
+public class Todos {
+    //TODO set player turn based on who plays
+    //TODO make automatic setting of colors of fields
+    //TODO fix animation of board
+    //TODO automatic determination of winner
+    //TODO load questions from database
+}
